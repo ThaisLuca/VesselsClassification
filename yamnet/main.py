937ee14bb650679c313d99627ca14ded602e07f4
= <thais.almeida@radixeng.com.br>
2019-12-01 12:56:32 -0300

    Creating new methods for kfold and plots

--- yamnet/main.py
+++ yamnet/main.py
@@ -17,8 +17,10 @@ import keras
 from keras.models import Sequential, Model
 from keras.layers import Dense, Activation, Input
 from keras import backend as K
-from matplotlib import pyplot
 
+from matplotlib import pyplot as plt
+
+import keras.metrics
 from keras.utils import to_categorical
 
 classes = [0,1,2,3]
@@ -47,62 +49,7 @@ def pre_processing(filenames):
 			labels.append(label)
 	return inputs, labels
 
-# for I in range(len(audio)/tamanho_entrada):
-#	inputs.append(audio[I*tamanho_entrada:I*(tamanho_entrada+1)])
-
-def get_model():
-	# Build MLP
-	visible1 = Input(shape=(1024,))
-	hidden1 = Dense(1024, activation='relu')(visible1)
-	hidden2 = Dense(512, activation='relu')(hidden1)
-	output = Dense(4, activation='softmax')(hidden2)
-	model = Model(inputs=visible1, outputs=output)
-	model.compile(optimizer='Adam', loss='mean_squared_error', metrics=['accuracy', 'mse'])
-
-	return model
-
-def main():
-
-	f_X_train = 0
-	f_y_train = 1
-	f_X_val = 2
-	f_y_val = 3
-
-	all_files = get_files_path()[4:]
-
-	# Build network
-	yamnet = yamnet_model.yamnet_frames_model(params)
-	yamnet.load_weights('yamnet.h5')
-	yamnet_classes = yamnet_model.class_names('yamnet_class_map.csv')
-
-	get_feature_layer_output = K.function([yamnet.layers[0].input], [yamnet.layers[-3].output])
-
-	waveforms = {}
-	labels = []
-	for file in all_files:
-		 # Decode the WAV file.
-		wav_data, sr = sf.read(file, dtype=np.int16)
-		assert wav_data.dtype == np.int16, 'Bad sample type: %r' % wav_data.dtype
-		waveform = wav_data / 32768.0  # Convert to [-1.0, +1.0]
-		label = file.split('\\')[-2][-1]
-		label = labels_dict[label]
-		print(label)
-
-		# Convert to mono and the sample rate expected by YAMNet.
-		if len(waveform.shape) > 1:
-			waveform = np.mean(waveform, axis=1)
-		if sr != params.SAMPLE_RATE:
-			waveform = resampy.resample(waveform, sr, params.SAMPLE_RATE)
-
-		avg = len(waveform) / float(5)
-		last = 0.0
-		waveforms[label] = []
-
-		while last < len(waveform):
-			waveforms[label].append(waveform[int(last):int(last + avg)])
-			labels.append(label)
-			last += avg
-
+def build_folds_test(waveforms, labels):
 	X_test = []
 	y_test = []
 	for c in classes:
@@ -211,7 +158,95 @@ def main():
 	fold_5.append(X_val)
 	fold_5.append(y_val)
 
-	folds = [fold_1, fold_2, fold_3, fold_4, fold_5]
+	return [fold_1, fold_2, fold_3, fold_4, fold_5], X_test, y_test
+
+# for I in range(len(audio)/tamanho_entrada):
+#	inputs.append(audio[I*tamanho_entrada:I*(tamanho_entrada+1)])
+
+def get_model():
+	# Build MLP
+	visible1 = Input(shape=(1024,))
+	hidden1 = Dense(1024, activation='relu')(visible1)
+	hidden2 = Dense(512, activation='relu')(hidden1)
+	output = Dense(4, activation='softmax')(hidden2)
+	model = Model(inputs=visible1, outputs=output)
+	model.compile(optimizer='Adam', loss='mean_squared_error', metrics=['accuracy', 'mse', keras.metrics.Precision()])
+
+	return model
+
+def plot(train_scores, test_scores, epochs):
+	param_range = np.arange(1, epochs, 1)
+
+	# Calculate mean and standard deviation for training set scores
+	train_mean = np.mean(train_scores, axis=1)
+	train_std = np.std(train_scores, axis=1)
+
+	# Calculate mean and standard deviation for test set scores
+	test_mean = np.mean(test_scores, axis=1)
+	test_std = np.std(test_scores, axis=1)
+
+	# Plot mean accuracy scores for training and test sets
+	plt.plot(param_range, train_mean, label="Training score", color="black")
+	plt.plot(param_range, test_mean, label="Cross-validation score", color="dimgrey")
+
+	# Plot accurancy bands for training and test sets
+	plt.fill_between(param_range, train_mean - train_std, train_mean + train_std, color="gray")
+	plt.fill_between(param_range, test_mean - test_std, test_mean + test_std, color="gainsboro")
+
+	# Create plot
+	plt.title("Validation Curve With Yamnet")
+	plt.xlabel("Epochs")
+	plt.ylabel("Accuracy Score")
+	plt.tight_layout()
+	plt.legend(loc="best")
+	plt.show()
+
+def main():
+	all_accuracy = []
+	all_loss = []
+
+	epochs=10
+
+	f_X_train = 0
+	f_y_train = 1
+	f_X_val = 2
+	f_y_val = 3
+
+	all_files = get_files_path()[4:]
+
+	# Build network
+	yamnet = yamnet_model.yamnet_frames_model(params)
+	yamnet.load_weights('yamnet.h5')
+	yamnet_classes = yamnet_model.class_names('yamnet_class_map.csv')
+
+	get_feature_layer_output = K.function([yamnet.layers[0].input], [yamnet.layers[-3].output])
+
+	waveforms = {}
+	labels = []
+	for file in all_files:
+		 # Decode the WAV file.
+		wav_data, sr = sf.read(file, dtype=np.int16)
+		assert wav_data.dtype == np.int16, 'Bad sample type: %r' % wav_data.dtype
+		waveform = wav_data / 32768.0  # Convert to [-1.0, +1.0]
+		label = file.split('\\')[-2][-1]
+		label = labels_dict[label]
+
+		# Convert to mono and the sample rate expected by YAMNet.
+		if len(waveform.shape) > 1:
+			waveform = np.mean(waveform, axis=1)
+		if sr != params.SAMPLE_RATE:
+			waveform = resampy.resample(waveform, sr, params.SAMPLE_RATE)
+
+		avg = len(waveform) / float(5)
+		last = 0.0
+		waveforms[label] = []
+
+		while last < len(waveform):
+			waveforms[label].append(waveform[int(last):int(last + avg)])
+			labels.append(label)
+			last += avg
+
+	folds, X_test, y_test = build_folds_test(waveforms, labels)
 
 	count = 1
 	for fold in folds:
@@ -247,17 +282,8 @@ def main():
 		print(X.shape, Y.shape)
 		count += 1
 
-		history = model.fit(X, Y, epochs=10, batch_size=32)
-		# plot metrics
-		#pyplot.plot(history.history['accuracy'])
-		#pyplot.show()
+		history_train = model.fit(X, Y, epochs=epochs, batch_size=32, validation_data=(X_V, Y_V))
 		
-		print(X_V.shape, Y_V.shape)
-		history = model.evaluate(X_V, Y_V)
-		print(history)
-		#pyplot.plot(history)
-		#pyplot.show()
-
+		plot(history_train.history['accuracy'], history_test[1], epochs)
+		
 	return
-
-main()
