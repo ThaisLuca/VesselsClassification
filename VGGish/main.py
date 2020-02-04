d3ddbb93519c3a054cc32f53c61ece77ae0e1174
= <thais.almeida@radixeng.com.br>
2020-02-04 15:43:24 -0300

    Adjusting kfold

--- VGGish/main.py
+++ VGGish/main.py
@@ -167,71 +167,70 @@ def main(argv):
 
     Y_V = to_categorical(Y_V)
 
-  with tf.Graph().as_default(), tf.Session() as sess:
-    # Define VGGish.
-    embeddings = vggish_slim.define_vggish_slim(FLAGS.train_vggish)
+    with tf.Graph().as_default(), tf.Session() as sess:
+      # Define VGGish.
+      embeddings = vggish_slim.define_vggish_slim(FLAGS.train_vggish)
 
-    # Define a shallow classification model and associated training ops on top
-    # of VGGish.
-    with tf.variable_scope('mymodel'):
-      # Add a fully connected layer with 100 units.
-      num_units = 100
-      fc = slim.fully_connected(embeddings, num_units)
+      # Define a shallow classification model and associated training ops on top
+      # of VGGish.
+      with tf.variable_scope('mymodel'):
+        # Add a fully connected layer with 100 units.
+        num_units = 100
+        fc = slim.fully_connected(embeddings, num_units)
 
-      # Add a classifier layer at the end, consisting of parallel logistic
-      # classifiers, one per class. This allows for multi-class tasks.
-      logits = slim.fully_connected(
-          fc, _NUM_CLASSES, activation_fn=None, scope='logits')
-      tf.sigmoid(logits, name='prediction')
+        # Add a classifier layer at the end, consisting of parallel logistic
+        # classifiers, one per class. This allows for multi-class tasks.
+        logits = slim.fully_connected(
+            fc, _NUM_CLASSES, activation_fn=None, scope='logits')
+        tf.sigmoid(logits, name='prediction')
 
-      # Add training ops.
-      with tf.variable_scope('train'):
-        global_step = tf.Variable(
-            0, name='global_step', trainable=False,
-            collections=[tf.GraphKeys.GLOBAL_VARIABLES,
-                         tf.GraphKeys.GLOBAL_STEP])
+        # Add training ops.
+        with tf.variable_scope('train'):
+          global_step = tf.Variable(
+              0, name='global_step', trainable=False,
+              collections=[tf.GraphKeys.GLOBAL_VARIABLES,
+                           tf.GraphKeys.GLOBAL_STEP])
 
-        # Labels are assumed to be fed as a batch multi-hot vectors, with
-        # a 1 in the position of each positive class label, and 0 elsewhere.
-        labels = tf.placeholder(
-            tf.float32, shape=(None, _NUM_CLASSES), name='labels')
+          # Labels are assumed to be fed as a batch multi-hot vectors, with
+          # a 1 in the position of each positive class label, and 0 elsewhere.
+          labels = tf.placeholder(
+              tf.float32, shape=(None, _NUM_CLASSES), name='labels')
 
-        # Cross-entropy label loss.
-        xent = tf.nn.sigmoid_cross_entropy_with_logits(
-            logits=logits, labels=labels, name='xent')
-        loss = tf.reduce_mean(xent, name='loss_op')
-        tf.summary.scalar('loss', loss)
+          # Cross-entropy label loss.
+          xent = tf.nn.sigmoid_cross_entropy_with_logits(
+              logits=logits, labels=labels, name='xent')
+          loss = tf.reduce_mean(xent, name='loss_op')
+          tf.summary.scalar('loss', loss)
 
-        # We use the same optimizer and hyperparameters as used to train VGGish.
-        optimizer = tf.train.AdamOptimizer(
-            learning_rate=vggish_params.LEARNING_RATE,
-            epsilon=vggish_params.ADAM_EPSILON)
-        optimizer.minimize(loss, global_step=global_step, name='train_op')
+          # We use the same optimizer and hyperparameters as used to train VGGish.
+          optimizer = tf.train.AdamOptimizer(
+              learning_rate=vggish_params.LEARNING_RATE,
+              epsilon=vggish_params.ADAM_EPSILON)
+          optimizer.minimize(loss, global_step=global_step, name='train_op')
 
-    # Initialize all variables in the model, and then load the pre-trained
-    # VGGish checkpoint.
-    sess.run(tf.global_variables_initializer())
-    vggish_slim.load_vggish_slim_checkpoint(sess, FLAGS.checkpoint)
+      # Initialize all variables in the model, and then load the pre-trained
+      # VGGish checkpoint.
+      sess.run(tf.global_variables_initializer())
+      vggish_slim.load_vggish_slim_checkpoint(sess, FLAGS.checkpoint)
 
-    # Locate all the tensors and ops we need for the training loop.
-    features_tensor = sess.graph.get_tensor_by_name(
-        vggish_params.INPUT_TENSOR_NAME)
-    labels_tensor = sess.graph.get_tensor_by_name('mymodel/train/labels:0')
-    global_step_tensor = sess.graph.get_tensor_by_name(
-        'mymodel/train/global_step:0')
-    loss_tensor = sess.graph.get_tensor_by_name('mymodel/train/loss_op:0')
-    train_op = sess.graph.get_operation_by_name('mymodel/train/train_op')
+      # Locate all the tensors and ops we need for the training loop.
+      features_tensor = sess.graph.get_tensor_by_name(
+          vggish_params.INPUT_TENSOR_NAME)
+      labels_tensor = sess.graph.get_tensor_by_name('mymodel/train/labels:0')
+      global_step_tensor = sess.graph.get_tensor_by_name('mymodel/train/global_step:0')
+      loss_tensor = sess.graph.get_tensor_by_name('mymodel/train/loss_op:0')
+      train_op = sess.graph.get_operation_by_name('mymodel/train/train_op')
 
-    # The training loop.
-    for _ in range(FLAGS.num_batches):
-      (features, labels) = X, Y
-      [num_steps, loss, _] = sess.run(
-          [global_step_tensor, loss_tensor, train_op],
-          feed_dict={features_tensor: features, labels_tensor: labels})
-      print('Step %d: loss %g' % (num_steps, loss))
+      # The training loop.
+      for _ in range(EPOCHS):
+        (features, labels) = X, Y #[num_steps, loss, _]
+        resut = sess.run(
+            [global_step_tensor, loss_tensor, train_op],
+            feed_dict={features_tensor: features, labels_tensor: labels})
+        #print('Step %d: loss %g' % (num_steps, loss))
+        print(result)
 
-
-      #TODO: medidas de acurácia, precisão e recall
+        #TODO: medidas de acurácia, precisão e recall
 
   return
 
